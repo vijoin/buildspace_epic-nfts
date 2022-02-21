7bed6827c9b56d9262012cedce4f2869606532ca
Victor Inojosa <vijoin@gmail.com>
2022-02-21 06:28:01 -0300

    Create basic contract and script to get started

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: GPL-3
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is my NFT contract. Yei!");
+    }
+}
